a179e8478b2de40fc220525af9c3919ed6c76a22
Anshika Singhal <singhalanshika82@gmail.com>
2026-07-05 18:52:24 +0530

    Initial Spring Boot project setup

---
+++ careerforge-api/src/main/java/com/careerforge/careerforge_api/CareerforgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.careerforge.careerforge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerforgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerforgeApiApplication.class, args);
+	}
+
+}
